579de4080beba2c731504c3c01f4bd8d2c0f4af5
Szabina Boli <szabina.boli@skola.sala.se>
2024-04-15 11:05:32 +0200

    Add project files.

---
+++ boink/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boink
+{
+    internal class BankAccount
+    {
+        int money = 0;
+     
+        public void Deposit(int amount)
+        {
+            money = money + amount;
+
+
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (money < amount)
+            {
+                Console.WriteLine("You dont have enough money.. ): this is all you have:" + money);
+            }
+            if (money > amount)
+            {
+                money = money - amount;
+            }
+            
+        }
+
+        public int GetBalance()
+        {
+            return money;
+        }
+    }
+}
---
+++ boink/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace boink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount1 = new BankAccount();
+            BankAccount bankAccount2 = new BankAccount();
+            bankAccount1.Deposit(int.Parse(Console.ReadLine()));
+            bankAccount2.Deposit(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Account1: " + bankAccount1.GetBalance());
+            Console.WriteLine("Account2: " + bankAccount2.GetBalance());
+            bankAccount1.Withdraw(int.Parse(Console.ReadLine()));
+            bankAccount2.Deposit(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Account1: " + bankAccount1.GetBalance());
+            Console.WriteLine("Account2: " + bankAccount2.GetBalance());
+            bankAccount1.Withdraw(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Account1: " + bankAccount1.GetBalance());
+
+        }
+    }
+}
